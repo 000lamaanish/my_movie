db47e737b3b8b89c3f17581c9d91a45956d4bec9
000lamaanish <astonishlama111@gmail.com>
2025-03-12 12:34:50 +0545

    Update Login.jsx using react hook

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,41 +1,5 @@
-import React, { useState } from 'react';
-import '../css/login.css'; // Make sure the CSS file is correctly linked
-
-const Login = () => {
-    const [formType, setFormType] = useState("Sign In");
-
-    return (
-        <div className="login-container">
-            <div className="login-card">
-                <h1>{formType}</h1>
-                <form className="login-form">
-                    {formType === "Sign Up" && (
-                        <input type="text" placeholder="Your Name" className="input-field" />
-                    )}
-                    <input type="email" placeholder="Email" className="input-field" />
-                    <input type="password" placeholder="Password" className="input-field" />
-
-                    <button type="submit" className="submit-btn">{formType}</button>
-
-                    <div className="extra-options">
-                        <label className="remember-me">
-                            <input type="checkbox" /> Remember Me
-                        </label>
-                        <p className="help-text">Need help?</p>
-                    </div>
-
-                    <p className="toggle-text">
-                        {formType === "Sign In"
-                            ? "New to our website? "
-                            : "Already have an account? "}
-                        <span onClick={() => setFormType(formType === "Sign In" ? "Sign Up" : "Sign In")}>
-                            {formType === "Sign In" ? "Sign Up" : "Sign In"}
-                        </span>
-                    </p>
-                </form>
-            </div>
-        </div>
-    );
-};
-
-export default Login;
+.form-input:focus {
+  border-color: #ff4757;
+  box-shadow: 0px 0px 8px rgba(255, 71, 87, 0.5);
+  outline: none;
+}
