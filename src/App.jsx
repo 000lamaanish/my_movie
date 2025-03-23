233ef20a9ab7bc13804bedd69c36038ba99f193d
000lamaanish <astonishlama111@gmail.com>
2025-03-23 21:40:53 +0545

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./css/App.css"
-// import Moviecard from "./component/moviecard"
-import Homepage from './pages/homepage'
+import Homepage from './pages/Homepage'
 import Favourite from './pages/fav'
 import Login from './login/Login'
 import Recommend from './pages/recommend'
-// import MovieDetail from './pages/MovieDetail'
+import Moviedetails from './component/Moviedetail'
 import { Routes, Route } from 'react-router-dom'
 const App = () => {
 
@@ -17,7 +16,8 @@ const App = () => {
           <Route path="/fav" element={<Favourite />} />
           <Route path="/login" element={<Login />} />
           <Route path="/recommend" element={<Recommend />} />
-          {/* <Route path="/movie/:id" element={<MovieDetail />} /> */}
+          <Route path="/movie/:id" element={<Moviedetails />} />
+
         </Routes>
       </main>
 
